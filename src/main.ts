c2a1361c107ab718f54812559c106ba614d95a7e
Saniul Ahsan <saniul.ahsan.12@gmail.com>
2021-02-03 17:05:09 +0600

    linting

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
