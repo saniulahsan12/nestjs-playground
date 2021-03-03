a8fcac5e4190bb305ba8be1c00e4deb637ed3380
Saniul Ahsan <saniul@binate-solutions.com>
2021-03-03 14:46:42 +0600

    sse implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,23 @@
 import { UserDTO } from './users/dto/user.dto';
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards, Sse, MessageEvent } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { ApiBody } from '@nestjs/swagger';
 
+import { Observable, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
+
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return of({ data: { hello: 'world' } });
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 
@@ -14,15 +15,20 @@ import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { LogsModule } from './logs/logs.module';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*'],
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'root',
+      username: 'admin',
+      password: 'admin',
       database: 'nest_db',
       entities: [Cats, Users],
       synchronize: true,
